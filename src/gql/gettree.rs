51abdfdc0bcf0492418487dd56d7896cb3d549d0
mimi <allthatjazz@outlook.com>
2020-12-11 20:51:42 -0500

    Organize tests

--- src/gql/gettree.rs
+++ src/gql/gettree.rs
@@ -72,3 +72,75 @@ impl graphql_client::GraphQLQuery for Gettree {
         }
     }
 }
+
+pub mod request {
+    use super::query::{GettreeRepositoryObjectOn, ResponseData, Variables};
+    use graphql_client::{GraphQLQuery, Response};
+    use reqwest::blocking::Client;
+    pub fn req_entries(url: String, token: String, repo_info: Variables) -> Vec<String> {
+        let query_body = super::Gettree::build_query(repo_info);
+        let client = Client::builder()
+            .user_agent("sir_rust/0.1.0")
+            .build()
+            .expect("client build failed");
+        let req_builder = client.post(&url).bearer_auth(token).json(&query_body);
+        let res = req_builder.send();
+        let response = res.expect("Resposne failed");
+        let res_body: Response<ResponseData> = response.json().expect("text failed");
+        let res_data: ResponseData = res_body.data.expect("data from body failed");
+        let obj = res_data
+            .repository
+            .as_ref()
+            .map(|repo| repo.object.as_ref())
+            .flatten()
+            .expect("repo object failed");
+        match &obj.on {
+            GettreeRepositoryObjectOn::Tree(x) => x
+                .entries
+                .as_ref()
+                .iter()
+                .flat_map(|xs| xs.iter())
+                .map(|o| (&o.name).clone())
+                .fold(vec![], |mut acc, n| {
+                    acc.push(n.to_string());
+                    acc
+                }),
+            _ => vec![],
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use crate::gql::gettree::query::Variables;
+        use regex::Regex;
+        use std::env;
+        #[test]
+        fn filenames() {
+            let url = "https://api.github.com/graphql";
+            let repo = "COVID-19".to_string();
+            let owner = "CSSEGISandData".to_string();
+            let folderpath = "master:csse_covid_19_data/csse_covid_19_daily_reports_us".to_string();
+            let args: env::Vars = env::vars();
+            let token = args
+                .filter(|(k, _)| k == "GHTOKEN1")
+                .map(|(_, v)| v)
+                .next()
+                .expect("no token env");
+            let filenames: Vec<String> = super::req_entries(
+                url.to_string(),
+                token,
+                Variables {
+                    owner,
+                    repo,
+                    folderpath,
+                },
+            );
+            let re = Regex::new(r"^\d{2}-\d{2}-\d{4}.csv$").expect("regex failure");
+            filenames
+                .iter()
+                .skip(20)
+                .take(10)
+                .for_each(|x| assert!(re.is_match(x)))
+        }
+    }
+}
