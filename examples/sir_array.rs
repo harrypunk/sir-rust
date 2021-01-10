86864fa0bd999700d0f688cc55fb9c050908df92
mimi <one@example.com>
2021-01-10 13:22:29 -0500

    Update example of sir

--- examples/sir_array.rs
+++ examples/sir_array.rs
@@ -2,17 +2,18 @@ use sir_rust::algorithm::sir::Euler;
 use sir_rust::algorithm::{Coefficients, InitialValue, Method, SimResult};
 
 pub fn main() {
+    let population = 500000;
     let init_val1 = InitialValue {
-        n: 1000,
-        s0: 999,
-        i0: 10,
-        r0: 2,
+        n: population,
+        s0: 400000,
+        i0: 100000,
+        r0: 10,
     };
     let euler = Euler {
         init_val: init_val1,
         rates: Coefficients {
-            infection_rate: 0.3,
-            recover_rate: 0.5,
+            infection_rate: 0.5,
+            recover_rate: 0.05,
         },
     };
 
@@ -20,9 +21,12 @@ pub fn main() {
 
     let result1: SimResult = euler.calc(days);
 
+    let n: f64 = population as f64;
     result1
         .data
         .iter()
+        .map(|(s, i, r)| (s * n, i * n, r * n))
+        .map(|(s, i, r)| (s as u32, i as u32, r as u32))
         .enumerate()
-        .for_each(|(i, sir)| println!("day: {}, sir: {}", i, sir.0));
+        .for_each(|(i, sir)| println!("day: {}, sir: {:?}", i, sir));
 }
